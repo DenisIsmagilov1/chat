7b6f7ab15a2742961aec0876d78a0909bd1afa14
Makxim Ismagilov <maksoneeeek@mail.ru>
2020-11-16 22:12:01 +0700

    fix folders

--- src/store/modules/folders.js
+++ src/store/modules/folders.js
@@ -3,9 +3,9 @@ import Api from '../../services/api';
 export default {
   state: {
     mailFolders: [
-      { id: 100, name: 'Все диалоги', type: "total", img: './static/img/all folders-grey.svg', imgHover: './static/img/all folders.svg' },
       { id: 101, name: 'Непрочитанные', type: "unread", img: './static/img/close mail-grey.svg', imgHover: './static/img/close mail.svg' },
       { id: 102, name: 'Прочитанные', type: "read", img: './static/img/open mail-grey.svg', imgHover: './static/img/open mail.svg' },
+      { id: 100, name: 'Все диалоги', type: "total", img: './static/img/all folders-grey.svg', imgHover: './static/img/all folders.svg' },
     ],
     programFolders: [
       { id: 200, name: 'Telegram', program: "TL", img: './static/img/telega.png' },
